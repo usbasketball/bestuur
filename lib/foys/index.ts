086871cb7fcaf815173ad9091f49ac802f75c3e0
Hsin <hi@hcheng.nl>
2026-09-03 15:25:52 +0200

    Fix foysMemberUrl path

--- lib/foys/index.ts
+++ lib/foys/index.ts
@@ -3,7 +3,7 @@ export const FOYS_CLUB_ID = "2f1e5e8e-e2c5-4d8b-9d21-1584bc6c8d5a";
 export const FOYS_BASE_URL = `https://club.basketball.nl/management/${FOYS_CLUB_ID}`;
 
 export function foysMemberUrl(foysUserId: string): string {
-  return `${FOYS_BASE_URL}/person/${foysUserId}`;
+  return `${FOYS_BASE_URL}/management/person/${foysUserId}`;
 }
 
 export function foysTeamUrl(foysTeamId: number): string {
