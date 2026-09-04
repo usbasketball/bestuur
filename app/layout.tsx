85748f70675e1cf58b9a542bf4f231047b1c5205
chengh42 <hi@hcheng.nl>
2026-09-04 17:59:26 +0200

    Set up Vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "U.S. Bestuur",
@@ -11,6 +12,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     <>
       {children}
       <Analytics />
+      <SpeedInsights />
     </>
   );
 }
